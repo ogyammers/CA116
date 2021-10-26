f195a40b60d69793643f717ac6b0c1b80dbfd4f4
ogyammers <jamigo@INSPIRON-7400.localdomain>
2021-10-26 21:50:47 +0100

    update

---
+++ lab05-test/ex1-hailstone.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+a = int(input())
+b = int(input())
+
+if b == (a / 2) or b == 3 * a + 1:
+   print("yes")
+else:
+   print("no")
---
+++ lab05-test/ex1-odd-or-even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+i = 0
+while i < 10:
+   n = int(input())
+   if (n % 2) == 0:
+      print("even")
+   else:
+      print("odd")
+   i = i + 1
